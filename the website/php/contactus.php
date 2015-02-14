f75d33c2bb0829f7723c3d626d6eb027a47a6664
tarekshumaila <shebatech@Dhaifs-iMac.local>
2015-02-14 07:25:30 -0700

    Sat morning edits
    Added svg repo in so we can use ready use vector images
    ‘—
    Adjusted footer content and contact us… need to make it display
    properly in contact us.
    
    Yay we’re almost done, one more week i think we’ll launch it.
    
    I will also create a to do list on the root folder to help with the
    things that needs to be done. As soon as we finish something from the
    list, we can scratch it. If we find something that needs to be done, we
    can add it.

--- the website/php/contactus.php
+++ the website/php/contactus.php
@@ -1,4 +1,14 @@
 <div class="title"> <img src="img/contactus.png"> </div>
+<ul id="socialbuttons">
+    <a href="#"><img src="img/footericons-01.png" class="footericon"></a>
+    <a href="#"><img src="img/footericons-02.png" class="footericon"></a>
+    <a href="#"><img src="img/footericons-03.png" class="footericon"></a>
+</ul>
+<ul id="socialbuttons">
+    <a href="#"><img src="../img/services/mail.png" class="footericon">info@shebatech.com</a>
+    <a href="#"><img src="img/footericons-02.png" class="footericon">+1(403)890-8700</a>
+    <a href="#"><img src="../img/services/maps.png" class="footericon">193 Hidden Creek Rd NW</a>
+</ul>
 <form id="form">
     <ul>
         <input type="text" id="formname" placeholder="Your Name"/>
