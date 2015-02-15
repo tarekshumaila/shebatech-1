33f471138474b232619b18d5efaef95a5435be17
tarekshumaila <shebatech@Dhaifs-iMac.local>
2015-02-15 10:17:26 -0700

    **Resolved** menu resizing and all
    smooth animation and all… done deal with the menu
    
    we need to check it out for mobile and make sure it doesn’t break :/

--- the website/php/menu.php
+++ the website/php/menu.php
@@ -7,24 +7,10 @@
 <li id="menu">
     <ul>
         <a href="#" class="menuitem first"> <li class="menu">Home</li> </a>
-        <a href="#" class="menuitem second"> <li class="menu">About us</li>  </a>
-        <a href="#" class="menuitem third"> <li class="menu">Services</li>  </a>
-        <a href="#" class="menuitem fourth"> <li class="menu">Portfolio</li>  </a>
-        <a href="#" class="menuitem fifth"> <li class="menu">Contact us</li>  </a>
+        <a href="#aboutus" class="menuitem second"> <li class="menu">About us</li>  </a>
+        <a href="#services" class="menuitem third"> <li class="menu">Services</li>  </a>
+        <a href="#portfolio" class="menuitem fourth"> <li class="menu">Portfolio</li>  </a>
+        <a href="#contactus" class="menuitem fifth"> <li class="menu">Contact us</li>  </a>
     </ul>
 </li>
-<script type="text/javascript">
-$( window ).scroll(function() {
-  $("#menucontainer").animate({height:'80px'}, 500);
-  $(".menuitem").animate({height:'80px'}, 500);
-  $(".menuitem").animate({width:'95px'}, 500);
-  $("#logo").animate({height:'80px'}, 500);
-  $("#menu").animate({height:'80px'}, 500);
-  $("#menu ul").animate({height:'80px'}, 500);
-  $("#logocontainer").animate({height:'80px'}, 500);
-
-  $(".menu").animate({'line-height':'90px'}, 500);
-  $(".menu").animate({'font-size':'16px'}, 500);
-
-});
-</script>
+<script src="../js/menuanimation.js"></script>
