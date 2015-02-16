8d6cf88842598048af3365d085ec16317aee49e8
tarekshumaila <tarekshumaila@Tareks-MacBook-Air.local>
2015-02-15 19:01:33 -0700

    Did some menu mobile fixes
    now the menu becomes smaller when you resize the screen. up to 468px
    width. not sure if we need to go more or not but it’s pretty small as
    it is now.
    
    — last polishing here include:
    1. hover is too big on mobile

--- the website/js/menuanimation.js
+++ the website/js/menuanimation.js
@@ -27,28 +27,5 @@ $(window).scroll(function(){
             }  
         }
     }
-    if ($(window).width() > 720 && $(window).width() < 1040  ) {
-        if($(document).scrollTop() > 0)
-        {
-            if($('#menucontainer').data('size') == 'big')
-            {
-                $('#menucontainer').data('size','small');
-                $(".menu").animate({'line-height':'70px','font-size':'14px'}, 500);
-                $("#menucontainer").animate({height:'60px'}, 500);
-                $(".menuitem").animate({width:'95px', height:'60px'}, 500);
-                $("#logo").animate({height:'60px'}, 500);
-            }
-        }
-        else
-        {
-            if($('#menucontainer').data('size') == 'small')
-            {
-                $('#menucontainer').data('size','big');
-                $("#menucontainer").animate({height:'83px'}, 500);
-                $(".menuitem").animate({width:'95px', height:'97px'}, 500);
-                $(".menu").animate({'line-height':'70px','font-size':'16px'}, 500);
-                $("#logo").animate({height:'97px'}, 500);
-            }  
-        }
-    }
+    
 });
