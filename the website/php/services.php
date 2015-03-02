f23a23d7ae69741a0a5636bb518b965124e03cec
tarekshumaila <shebatech@Dhaifs-iMac.local>
2015-03-01 19:13:07 -0700

    fix mobile padding in the menu

--- the website/php/services.php
+++ the website/php/services.php
@@ -1,30 +1,25 @@
 <div class="mediumcontainer flex justify-around wrap-wrap">
 <ul class="title centerMeInANewLine"> <img  src="img/services.png"> </ul>
-
+    <ul class="servicecircle">
+        <ul class="serviceicon"> <img src="../img/services/cloud.png"> </ul>
+        <p class="servicetitle">Website Development</p>
+        <p class="servicedescription">Serving you the best variety of offerings to meet your project needs.</p>
+    </ul>
     <ul class="servicecircle">
         <ul class="serviceicon"> <img src="../img/services/graphicdesign.png"> </ul>
         <p class="servicetitle">Branding and Design</p>
         <p class="servicedescription">Improve your corporate image with our branding and design services</p>
     </ul>
-
     <ul class="servicecircle">
-        <ul class="serviceicon"> <img src="../img/services/maps.png"> </ul>
-        <p class="servicetitle">GIS Maps</p>
+        <ul class="serviceicon"><img src="../img/services/maps.png"></ul>
+        <p class="servicetitle">Apps Development</p>
         <p class="servicedescription">Visualize public data and layer it with your data to make intelligent descisions</p>
     </ul>
-
     <ul class="servicecircle">
-        <ul class="serviceicon"> <img src="../img/services/webdevelopment.png"> </ul>
-        <p class="servicetitle"> Web Development </p>
+        <ul class="serviceicon"><img src="../img/services/webdevelopment.png"></ul>
+        <p class="servicetitle">Web Development</p>
         <p class="servicedescription">Make the website that you have been dreaming about with us</p>
     </ul>
-
-    <ul class="servicecircle">
-        <ul class="serviceicon"> <img src="../img/services/cloud.png"> </ul>
-        <p class="servicetitle"> Cloud Solutions</p>
-        <p class="servicedescription">The technology is avaiable and affordable, why not use it and save?</p>
-    </ul>
-
     <ul class="servicecircle">
         <ul class="serviceicon"> <img src="../img/services/consultant.png"> </ul>
         <p class="servicetitle"> General Consulting </p>
@@ -33,7 +28,7 @@
 
     <ul class="servicecircle">
         <ul class="serviceicon"> <img src="../img/services/mail.png"> </ul>
-        <p class="servicetitle"> Email Solutions </p>
+        <p class="servicetitle">..next Robots</p>
         <p class="servicedescription">Whether it's Google services or Microsoft Exchange, we can assist you.</p>
     </ul>
 </div>
