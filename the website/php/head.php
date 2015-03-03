77001e59ec5a3a55d68f72d8eb1e61ec239d80e0
tarekshumaila <shebatech@Dhaifs-iMac.local>
2015-03-02 20:54:10 -0700

    removed old slider and replaced it with a new framework
    we’ll have to do it manually, just put a trigger to show and hide
    content.

--- the website/php/head.php
+++ the website/php/head.php
@@ -3,5 +3,4 @@
 </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" href="cssslider_files/csss_engine1//style.css">
 <link rel="stylesheet" href="css/ShebaFlex.css">
